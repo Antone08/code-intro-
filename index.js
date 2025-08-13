94c8dcbd39be4976e5f99732686c437ba1830474
Antonio <amp.olivas00@gmail.com>
2025-08-13 10:22:28 -0600

    added index.js file

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("Hello World")
+const readline = require('readline')
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  
+const name = "Antonio"
+
+let age = 23
+
+const isCool = true
+
+let car = null
+
+let height
+
+
+let x = 4
+
+let y = 10
+
+let z = 7 + 8
+
+let sum = x + y
+
+let str = "This is a string"
+
+let strTwo = "This is a second string"
+
+let strThree = `This is my name: ${name}, and this is my age: ${age}`
+
+let strings = str + strTwo + strThree
+
+console.log(strThree)
+
+reader.question("Howdy, what's your name?", function(answer) {
+    console.log("Hi " + answer);
+  });
+  
